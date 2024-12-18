90c78ad0714218527649558b2da344aeeb77a373
박선우 <dbxlf3018@gmail.com>
2024-12-18 15:20:18 +0900

    useReducer을 통해 생성,삭제 제어

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
 import './App.css'
 import { useState } from 'react'
-import { useRef } from 'react'
+import { useRef, useReducer } from 'react'
 
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
 
+
+//reducer로 변경
+function reducer(state, action) {
+  switch(action.type){
+    case "CREATE" : return [action.data,...state];
+    case "UPDATE" : return [action.data,...state];
+    case "DELETE" : return state.filter(data=>action.data.id != data.id);
+    default: return state;
+  }
+  
+}
+
 function App() {
 
-  //TodoList담을 state
-  const [todoList,setTodoList] = useState([]);
+  //reducer를 통해 데이터 조작
+  const [todoList,dispatch] = useReducer(reducer,[]);
+
   //Id값
   const todoId = useRef(0);
 
   //Todo 추가
   const createTodo = (content) => {
-
-    const todo = {
-      id:todoId.current++,
-      content:content,
-      date:new Date().toDateString()
-    };
-
-    setTodoList([todo,...todoList]);
+    dispatch({
+      type:"CREATE",
+      data:{
+        id:todoId.current++,
+        content:content,
+        date:new Date().toDateString()
+      }
+    })
   }
 
 
   //Todo 삭제
   const deleteTodo = (targetId) => {
-    setTodoList(todoList.filter((todo)=>{return todo.id!=targetId}));
+    dispatch({
+      type:"DELETE",
+      data:{id:targetId},
+    })
   }
 
   
